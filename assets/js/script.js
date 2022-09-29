10c44d54ccabc58bd4a9885012b3137caaf01fb0
etienne <etiennerodriguez3@gmail.com>
2022-09-29 08:59:23 -0400

    tried getting api key to work again, no success at all. hopefully what I have tried to do makes sense

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,74 @@
-var apiKey = "0c8811679fmshd4486e2104d36a4p1bd4abjsne0c793c0062a";
-var searchButton = document.querySelector("#search-btn");
+var dateText = document.querySelector('#display-date')
+
+var apiKey = "e04e3fdbd3f9d1194e400e14cc8f5a3f";
+var searchButton = document.querySelector("#searchbtn");
 var inputCity = document.querySelector("#city");
 
 inputCity.value = localStorage.getItem("cityName");
 
 searchButton.addEventListener("click", event => {
     localStorage.setItem("cityName", $('#city').val());
-})
+});
+
+var todayDate = new Date();
+var todayDay = currentDate.getDate();
+var todayMonth = currentDate.getMonth();
+var todayYear = currentDate.getFullYear();
+
+var displayDate = function () {
+    dateText.innerHTML =
+    "Today's date is " +
+    todayYear + '/' + todayMonth + '/' + todayDay;
+};
 
 
 
+const iconE = document.querySelector(".icon");
+const tempE = document.querySelector(".temperature p");
+const tempEDesc = document.querySelector(".temperature-desc p");
+const locationE = document.querySelector(".location p");
+
+//defining properties
+const weather = {
+
+    temperature: {
+        value: 16,
+        unit: "celsius"
+    },
+    description: "Thunderstorms",
+    icondId: "tstorms",
+    city: "Toronto",
+    country: "CA"
+};
+
+displayWeather(){
+
+iconE.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+
+tempE.innerHTML =  `${weather.temperature.value} ° C`;
+
+tempEDesc.innerHTML = weather.description;
+
+locationE.innerHTML = `${weather.city}, ${weather.country}`;
+};
+
+function celToFar(temperature){
+ (temperature * 9/5) + 32;
+}
+
+if("geolocation" in navigator){
+    navigator.geolocation.getCurrentPosition( setPosition, showError);
+} else {
+    window.alert("Browser does not support geolocation")
+}
+
+function setPosition(position){
+    let lat = position.coords.latitude;
+    let long = position.coords.longitude;
+    getWeather(lat, long);
+}
+
+function getWeather(lat, long){
+    let apiKeyLink = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`
+}
+// displayDate();
